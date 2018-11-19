6d5e07ed95c1189b52ae60b7c85d00e45fb02eca
kangazoom <michellelcronin@gmail.com>
2018-11-18 18:01:59 -0800

    added ability to score a word

--- src/adagrams.js
+++ src/adagrams.js
@@ -67,6 +67,51 @@ const Adagrams = {
     }
     // if we made it thru without returning false, the input is valid
     return true
+  },
+
+  scoreWord(word) {
+    const letter_score = {
+      "A": 1,
+      "B": 3,
+      "C": 3,
+      "D": 2,
+      "E": 1,
+      "F": 4,
+      "G": 2,
+      "H": 4,
+      "I": 1,
+      "J": 8,
+      "K": 5,
+      "L": 1,
+      "M": 3,
+      "N": 1,
+      "O": 1,
+      "P": 3,
+      "Q": 10,
+      "R": 1,
+      "S": 1,
+      "T": 1,
+      "U": 1,
+      "V": 4,
+      "W": 4,
+      "X": 8,
+      "Y": 4,
+      "Z": 10
+    };
+
+    let score_sum = 0
+    const special_length = [7,8,9,10]
+
+    if (special_length.includes(word.length)) {
+      score_sum += 8
+    }
+
+    for (let i=0; i < word.length; i++) {
+      let letter = word[i]
+      score_sum += letter_score[letter]
+    }
+
+    return score_sum
   }
 
 
@@ -75,7 +120,7 @@ const Adagrams = {
 
 
 
-  
+
 };
 
 // Do not remove this line or your tests will break!
